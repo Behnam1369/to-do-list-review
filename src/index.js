cfefa652ad8cf2a9ad0833d380b80610c32df2d2
Behnam Aghaali <Behnam.Aghaali@yahoo.com>
2022-02-21 23:12:28 +0330

    Add project files

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import TaskList from './taskList.js';
+import './style.css';
+import Task from './task.js';
+import './interaction.js';
+
+const tasks = (localStorage.tasks ? JSON.parse(localStorage.tasks) : []);
+
+const list = new TaskList(tasks);
+window.addEventListener('load', () => {
+  list.show();
+});
+
+const addTask = () => {
+  const txt = document.querySelector('.txtNew');
+  const description = txt.value;
+  if (description === '') {
+    alert('Please type your task description');
+  } else {
+    list.addTask(new Task(description));
+    txt.value = '';
+  }
+};
+
+document.querySelector('.add').addEventListener('click', () => {
+  addTask();
+});
+
+document.querySelector('.txtNew').addEventListener('keyup', (e) => {
+  if (e.keyCode === 13) { // Enter key
+    addTask();
+  }
+});
+
+document.querySelector('.remove').addEventListener('click', () => {
+  list.clearCompletedTasks();
+});
